bfb5204956575d75f0f2437a72deb72ef5a8a75b
Dave Harned <dharned@gmail.com>
2019-03-04 22:52:34 -0600

    setup and test public index page

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // web request server
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
