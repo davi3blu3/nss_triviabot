f6d69a0a2a528a07c02e4c3b8b0f4475fdda9878
Dave Harned <dharned@gmail.com>
2019-03-07 12:25:50 -0600

    troubleshoot function syntax

--- services/create.js
+++ services/create.js
@@ -1,24 +1,24 @@
-const createDayPoll = function(dayString) {
-  console.log('create dayPoll triggered');
-  return dayString;
-},
-
-const createVenuePoll = function(day) {
-  console.log('create venuePoll triggered');
-  return day;
-}
-
 module.exports = {
   poll: function(command) {
     // determine which poll to create
     if (command.slice(0, 3).toUpperCase() === 'DAY') {
-      return createDayPoll('MTWRF');
+      return this.createDayPoll('MTWRF');
     } else if (command.slice(0, 5).toUpperCase() === 'VENUE') {
-      return createVenuePoll('Thursday');
+      return this.createVenuePoll('Thursday');
     } else {
       console.log('problem with request text:', command);
       console.log(command.slice(0, 3).toUpperCase());
       console.log(command.slice(0, 5).toUpperCase());
     }
   },
+
+  createDayPoll: function(dayString) {
+    console.log('create dayPoll triggered');
+    return dayString;
+  },
+
+  createVenuePoll: function(day) {
+    console.log('create venuePoll triggered');
+    return day;
+  }
 };
