c3726b8e674c50ecdd4eb3c2a752ff3a3533aa23
Dave Harned <dharned@gmail.com>
2019-03-07 11:50:32 -0600

    switch validate to config vars

--- services/validate.js
+++ services/validate.js
@@ -1,13 +1,13 @@
 module.exports = {
   // validate the initial slash command request by comparing payload credentials
   initRequest: function(payload) {
-    if (payload.command !== env.payloadModel.command) {
+    if (payload.command !== process.env.slash_command) {
       console.log('error: slash command mismatch');
       return false;
-    } else if (payload.team_id !== env.payloadModel.team_id) {
+    } else if (payload.team_id !== process.env.team_id) {
       console.log('error: team_id mismatch');
       return false;
-    } else if (payload.team_domain !== env.payloadModel.team_domain) {
+    } else if (payload.team_domain !== process.env.team_domain) {
       console.log('error: team_domain mismatch');
       return false;
     } else {
