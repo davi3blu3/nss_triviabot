e13c2174e5535fbbb80dccb2b8381ac239374fdc
Dave Harned <dharned@gmail.com>
2019-03-07 11:57:08 -0600

    testing validation

--- services/validate.js
+++ services/validate.js
@@ -3,12 +3,15 @@ module.exports = {
   initRequest: function(payload) {
     if (payload.command !== process.env.slash_command) {
       console.log('error: slash command mismatch');
+      console.log(payload.command, process.env.slash_command);
       return false;
     } else if (payload.team_id !== process.env.team_id) {
       console.log('error: team_id mismatch');
+      console.log(payload.team_id, process.env.team_id);
       return false;
     } else if (payload.team_domain !== process.env.team_domain) {
       console.log('error: team_domain mismatch');
+      console.log(payload.team_domain, process.env.team_domain);
       return false;
     } else {
       console.log('request validation successful!');
